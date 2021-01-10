d2ae2f82c93e12fd2d91883ae0acc9fa3b968bd7
rafael-abuawad <rafael.abuawad@live.com>
2021-01-10 09:15:59 -0400

    Posts API controller uptated

--- src/modules/post/post.controller-api.js
+++ src/modules/post/post.controller-api.js
@@ -9,16 +9,24 @@ router
       const posts = await postService.find();
       res.json({ posts });
     } catch (error) {
-      res.json({ error });
+      console.error(error);
+      res.json({ error: "The request couldn't be completed" });
     }
   })
   .post(async (req, res) => {
     try {
-      const { title, content } = req.body;
-      const post = await postService.create(title, content);
+      // TODO: Change to JWT auth, get the Author ID from that
+      const { title, description, minutesPerBlock, authorId } = req.body;
+      const post = await postService.create(
+        title,
+        description,
+        minutesPerBlock,
+        authorId
+      );
       res.json({ post });
     } catch (error) {
-      res.json({ error });
+      console.error(error);
+      res.json({ error: "The request couldn't be completed" });
     }
   });
 
@@ -29,12 +37,13 @@ router
       const post = await postService.findById(req.params.id);
       res.json({ post });
     } catch (error) {
-      res.json({ error });
+      console.error(error);
+      res.json({ error: "The request couldn't be completed" });
     }
   })
   .put(async (req, res) => {
     try {
-      const { title, content, published } = req.body;
+      const { title, description, published } = req.body;
       const post = await postService.update(
         req.params.id,
         title,
@@ -43,7 +52,8 @@ router
       );
       res.json({ post });
     } catch (error) {
-      res.json({ error });
+      console.error(error);
+      res.json({ error: "The request couldn't be completed" });
     }
   })
   .delete(async (req, res) => {
@@ -51,8 +61,20 @@ router
       const post = await postService.remove(req.params.id);
       res.json({ post });
     } catch (error) {
-      res.json({ error });
+      console.error(error);
+      res.json({ error: "The request couldn't be completed" });
     }
   });
+
+router.put('/:id/increment', async (req, res) => {
+  try {
+    const { authorId } = req.body;
+    const post = await postService.updateCompleted(req.params.id, authorId);
+    res.json({ post });
+  } catch (error) {
+    console.error(error);
+    res.json({ error: "The request couldn't be completed" });
+  }
+});
 
 module.exports = router;
